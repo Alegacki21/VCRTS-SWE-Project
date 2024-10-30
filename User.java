1eb61fa4aa752c765f457a33cd355fea01bdf4e1
Keepas3 <bfun679@gmail.com>
2024-10-30 13:35:51 -0400

    Added classes on the LucidChart Diagram and variables/methods for user

---
+++ User.java
@@ -0,0 +1,77 @@
+public class User {
+    
+private String userId;
+private String email;
+private String username;
+private String name;
+private String password;
+private double balance;
+private String paymentMethod;
+
+public User(String userId, String email, String username, String name, String password, double balance, String paymentMethod) {
+    this.userId = userId;
+    this.email = email;
+    this.username = username;
+    this.name = name;
+    this.password = password;
+    this.balance = balance;
+    this.paymentMethod = paymentMethod;
+}
+
+public String getUserId() { 
+    return userId; 
+} 
+public String getEmail() { 
+    return email; 
+} 
+public void setEmail(String email) {
+     this.email = email; 
+    } 
+public String getUsername() {
+     return username; 
+    } 
+public void setUsername(String username) {
+     this.username = username; 
+    }
+public String getName() {
+    return name; 
+  } 
+public void setName(String name) {
+     this.name = name; 
+    } 
+public double getBalance() {
+    return balance; 
+    } 
+public void setBalance(double balance) {
+    this.balance = balance; 
+    }
+public String getPaymentMethod() {
+    return paymentMethod; 
+`}
+public void setPaymentMethod(String paymentMethod) {
+    this.paymentMethod = paymentMethod; 
+    }
+
+
+public void updateProfile() {
+
+}
+public void changePassword(String oldPassword, String newPassword) {
+
+}
+public void addFunds(double amount) {
+
+}
+public void withdrawFunds(double amount) {
+
+}
+public void viewBalance() {
+
+}
+public void login() {
+
+}
+public void logout() {
+
+}
+}
