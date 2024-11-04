99692f6aa567fb36f3ff0503d9c38ce42144c25a
allanilya <allanilyasov@gmail.com>
2024-11-03 21:47:04 -0500

    updated vehicle class

--- Vehicle.java
+++ Vehicle.java
@@ -1,6 +1,21 @@
+import java.time.*;
+
 public class Vehicle {
 
     public Vehicle() {
-        
+        String vehicleID;
+        VehicleOwner vehicleOwner;
+        String model;
+        int year;
+        String make;
+        String VIN;
+        double computationalPower;
+        double storageCapacity;
+        String status;
+        String location;
+        LocalDateTime residencyTime;
+        Boolean isAvailable;
+
+
     }
 }
