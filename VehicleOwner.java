38c1fc0536e6b012cd46843633837e6a55dd77bc
allanilya <allanilyasov@gmail.com>
2024-11-04 22:46:57 -0500

    register-vehicle

--- VehicleOwner.java
+++ VehicleOwner.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 public class VehicleOwner extends User {
 private List<Vehicle> vehicleList;
@@ -29,8 +32,37 @@ private String paymentAccount;
 
     //Methods
 
-    public void registerVehicle(Vehicle v) { // NEEDS TO BE IMPLEMENTED and stored in a file
-        vehicleList.add(new Vehicle());
+    public void registerVehicle(Vehicle v) {
+        vehicleList.add(v);
+        saveVehicle(v);
+    }
+
+    private void saveVehicle(Vehicle vehicle) {
+        String fileName = "registered_vehicles.txt"; 
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) { // Append mode
+            writer.write(formatVehicleData(vehicle));
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("Error saving vehicle to file: " + e.getMessage());
+        }
+    }
+    private String formatVehicleData(Vehicle vehicle) {
+        return String.join(",",
+                vehicle.getName(),
+                vehicle.getVehicleID(),
+                vehicle.getModel(),
+                Integer.toString(vehicle.getYear()),
+                vehicle.getMake(),
+                vehicle.getVIN(),
+                Double.toString(vehicle.getComputationalPower()),
+                Double.toString(vehicle.getStorageCapacity()),
+                vehicle.getStatus(),
+                vehicle.getLocation(),
+                vehicle.getArrivalTime().toString(),
+                vehicle.getDepartureTime().toString(),
+                Boolean.toString(vehicle.isAvailable())
+        );
     }
     public void specifyResources() {
 
