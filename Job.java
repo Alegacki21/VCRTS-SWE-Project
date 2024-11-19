9dfc0783f5d388755dcb8ad4fa1c691adb1ef0b7
Mathew Martinez <mathew.martinez21@my.stjohns.edu>
2024-11-18 21:28:17 -0500

    reverted incorrect merges from previous accidental commit

--- Job.java
+++ Job.java
@@ -1,25 +1,14 @@
 public class Job {
-    private String jobID;
-    private String jobSubmitterID;
-    private String status;
-    private Duration jobRunTime;
-    private int assignedVehicles;
-    private double computationalPowerNeeded;
-    private double storageNeeded;
-    private Duration estimatedDuration;
+    private String jobId;
+    private int duration;
     private int arrivalTime;
     private int completionTime;
+    private double computationalPowerNeeded;
+    private String status;
 
-    public Job(String jobID, String jobSubmitterID, String status, Duration jobRunTime, int assignedVehicles,
-               double computationalPowerNeeded, double storageNeeded, Duration estimatedDuration, int arrivalTime, int duration) {
-        this.jobID = jobID;
-        this.jobSubmitterID = jobSubmitterID;
-        this.status = status;
-        this.jobRunTime = jobRunTime;
-        this.assignedVehicles = assignedVehicles;
-        this.computationalPowerNeeded = computationalPowerNeeded;
-        this.storageNeeded = storageNeeded;
-        this.estimatedDuration = estimatedDuration;
+    public Job(String jobId, int duration, int arrivalTime) {
+        this.jobId = jobId;
+        this.duration = duration;
         this.arrivalTime = arrivalTime;
         this.status = "Pending";
     }
@@ -35,72 +24,5 @@ public class Job {
     public void setStatus(String status) { this.status = status; }
     public void setDuration(int duration) {
         this.duration = duration;
-        this.reason = reason;
-    }
-
-    //Getters and Setters
-
-    public String getJobID() {
-        return jobID;
-    }
-
-    public String getJobSubmitterID() {
-        return jobSubmitterID;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public Duration getJobRunTime() {
-        return jobRunTime;
-    }
-
-    public int getAssignedVehicles() {
-        return assignedVehicles;
-    }
-
-    public double getComputationalPowerNeeded() {
-        return computationalPowerNeeded;
-    }
-
-    public double getStorageNeeded() {
-        return storageNeeded;
-    }
-
-    public Duration getEstimatedDuration() {
-        return estimatedDuration;
-    }
-
-    public int getArrivalTime() {
-        return arrivalTime;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public int getCompletionTime() {
-        return completionTime;
-    }
-
-    public void setCompletionTime(int completionTime) {
-        this.completionTime = completionTime;
-    }
-
-    public void execute() {
-        
-    }
-
-    public void updateStatus(String newStatus) {
-        this.status = newStatus;
-    }
-
-    public void createCheckpoint() {
-       
-    }
-
-    public void restartFromCheckpoint(Checkpoint checkpoint) {
-       
     }
 }
--- JobSubmitter.java
+++ JobSubmitter.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import javax.swing.JDialog;
